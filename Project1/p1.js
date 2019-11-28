e373cb56e2128d03d58dd2eb3389a86203cef0dd
George-Marzouk <57626406+George-Marzouk@users.noreply.github.com>
2019-11-28 23:56:55 +0200

    Update p1.js

--- Project1/p1.js
+++ Project1/p1.js
@@ -2,7 +2,6 @@ function getRandom()
 {
 	return (Math.floor(Math.random()*26));
 }
-
 var nunmberOfLetters = document.getElementById('text');
 var botn = document.getElementById('btn');
 
@@ -14,41 +13,49 @@ var ButtonHolder = document.getElementById("div");
 var temp;
 // The event in the generate button
 botn.onclick = function(e){
-    var arr = [];
-	//freeing the div of the buttons in  every generation
-    ButtonHolder.textContent = "";
-    if(nunmberOfLetters.value <= 26 && nunmberOfLetters.value >0){
-        for(var i = 0;i < nunmberOfLetters.value;i++)
-		{
-            temp = getRandom();
-			// No repeating in letters
-            while(arr.indexOf(temp)!=-1)
-                {
-                    temp=getRandom();
-                }
-            arr[i]=temp;
-        }
-		// Generating the New Buttons
-        for(var i = 0;i < nunmberOfLetters.value;i++){   
-            var q = document.createElement("button");
-            var v = document.createTextNode(letters[arr[i]]);
-            q.appendChild(v);
-            ButtonHolder.appendChild(q);
-			// LocalStorge Storing
-            localStorage[letters[arr[i]]+" created"]=new Date();
-            
-        }    
-    }
-    localStorage[e.target.textContent]=new Date();
+	if(nunmberOfLetters.value!="")
+	{
+	if(nunmberOfLetters.value<=26 && nunmberOfLetters.value>0)
+	{
+		var arr = [];
+		//freeing the div of the buttons in  every generation
+		ButtonHolder.textContent = "";
+			for(var i = 0;i < nunmberOfLetters.value;i++)
+			{
+				temp = getRandom();
+				// Check repeating in letters
+				while(arr.indexOf(temp)!=-1)
+					{
+						temp=getRandom();
+					}
+				arr[i]=temp;
+			}
+			// Generating the New Buttons
+			for(var i = 0;i < nunmberOfLetters.value;i++){   
+				var q = document.createElement("button");
+				var v = document.createTextNode(letters[arr[i]]);
+				q.appendChild(v);
+				ButtonHolder.appendChild(q);
+				// LocalStorge Storing
+				localStorage["Button "+letters[arr[i]]+" Clicked"]=new Date();
+			}    
+		
+		localStorage[" Button "+e.target.value+" Clicked"]=new Date();
+	}else{
+		alert("You should enter number between 1 and 26");
+	}
+	}else{alert("Please enter a number in the empty Box");}
 };
 // Generating the Image
 ButtonHolder.addEventListener("click",function(e){
+	e.target.style.border="5px dashed white";
     var photo=document.getElementsByTagName("img")[0];
     photo.src=e.target.textContent+".gif";
     photo.setAttribute("style","width:300px; height:300px; margin:35px ;");
-    localStorage[e.target.textContent+" "+e.type]=new Date();
-    
-    
+    localStorage["Image "+e.target.textContent+" Created"]=new Date();
+
+	
+	
 });
 // LocalStorge Storing
 window.addEventListener("load",function(){
@@ -59,3 +66,5 @@ window.addEventListener("load",function(){
 window.addEventListener("unload",function(){
     localStorage["unload"]=new Date();
 });
+// Cleacing the localStorage
+setInterval(function(){localStorage.clear()},25000);
